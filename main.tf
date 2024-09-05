b768d53bffe78f0fb3add1f3c26a41f0a85c8f7c
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-05 12:41:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  project = "<your-gcp-project-id>"
+  region  = "us-central1"
+  credentials = file("<path-to-service-account-key>.json")
+}
+
+# Create a GKE Cluster
+resource "google_container_cluster" "primary" {
+  name     = "gke-cluster"
+  location = "us-central1"
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 3
+
+    node_config {
+      machine_type = "n1-standard-1"
+    }
+  }
+}
+
+# Create a Managed Instance Group (MIG)
+resource "google_compute_instance_template" "template" {
+  name         = "instance-template"
+  machine_type = "n1-standard-1"
+  source_image = "projects/debian-cloud/global/images/family/debian-10"
+}
+
+resource "google_compute_instance_group_manager" "mig" {
+  name               = "instance-group"
+  base_instance_name = "instance"
+  instance_template  = google_compute_instance_template.template.self_link
+  target_size        = 2
+  zone               = "us-central1-a"
+}
